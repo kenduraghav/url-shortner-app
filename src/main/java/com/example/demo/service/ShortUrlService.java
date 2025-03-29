5b5023b26f88f300f9331bf11797989dca2b65a9
kenduraghav <kenduraghav@gmail.com>
2025-03-29 21:18:44 +0800

    Added findByIsPrivateIsFalse method for listing only public urls

--- src/main/java/com/example/demo/service/ShortUrlService.java
+++ src/main/java/com/example/demo/service/ShortUrlService.java
@@ -1,8 +1,23 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.entity.ShortUrl;
+import com.example.demo.domain.repositories.ShortUrlRepository;
 
 @Service
 public class ShortUrlService {
 
+	private final ShortUrlRepository shortUrlRepository;
+	
+	public ShortUrlService(ShortUrlRepository shortUrlRepository) {
+		this.shortUrlRepository = shortUrlRepository;
+	}
+	
+	
+	public List<ShortUrl> findAllPublicUrls(){
+		return shortUrlRepository.findByIsPrivateIsFalse();
+	}
 }
--- src/main/java/com/example/demo/web/controller/HomeController.java
+++ src/main/java/com/example/demo/web/controller/HomeController.java
@@ -10,22 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.domain.entity.ShortUrl;
 import com.example.demo.domain.repositories.ShortUrlRepository;
+import com.example.demo.service.ShortUrlService;
 
 @Controller
 public class HomeController {
 
 	
-	private final ShortUrlRepository shortUrlRepository;
+	private final ShortUrlService shortUrlService;
 	
 	
-	public HomeController(ShortUrlRepository shortUrlRepository) {
-		this.shortUrlRepository = shortUrlRepository;
+	public HomeController(ShortUrlService shortUrlService) {
+		this.shortUrlService = shortUrlService;
 	}
 
 	@GetMapping("/")
 	public String home(Model model) {
-		
-		List<ShortUrl> shortUrls = shortUrlRepository.findAll(Sort.by(Direction.DESC, "createdAt"));
+		List<ShortUrl> shortUrls = shortUrlService.findAllPublicUrls();
 		model.addAttribute("shortUrls", shortUrls);
 		model.addAttribute("title", "URL Shortner App - using Thymeleaf");
 		return "index";
