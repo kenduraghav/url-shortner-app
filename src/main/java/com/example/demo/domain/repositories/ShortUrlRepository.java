0f86d26c4588a23c06b6d2f7f539f7fac5e7a60f
kenduraghav <kenduraghav@gmail.com>
2025-04-01 21:12:57 +0800

    Fixed lazy loading using second approach with Entity Graphs

--- src/main/java/com/example/demo/domain/repositories/ShortUrlRepository.java
+++ src/main/java/com/example/demo/domain/repositories/ShortUrlRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,8 @@ import com.example.demo.domain.entity.ShortUrl;
 @Repository
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
 
-	@Query("select e from ShortUrl e left join fetch e.createdBy where e.isPrivate = false order by e.createdAt desc")
+//	@Query("select e from ShortUrl e left join fetch e.createdBy where e.isPrivate = false order by e.createdAt desc")
+	@Query("select e from ShortUrl e where e.isPrivate = false order by e.createdAt desc")
+	@EntityGraph(attributePaths = {"createdBy"})
 	List<ShortUrl> findAllPublicUrls();
 }
